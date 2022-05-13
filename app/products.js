66ef3fbfe9df46841332e5d4ba27c6bebeba07dd
Stefano <stefano.pavan.22@gmail.com>
2022-05-13 15:22:05 +0200

    Metodo POST per aggiunta di un prodotto al catalogo

--- app/products.js
+++ app/products.js
@@ -6,8 +6,24 @@ const router = express.Router();
 const Product = require("./models/product");
 
 //
-// PARTE DEI METODI REST DA INSERIRE QUI
-router.get('', async (req, res) => {
-//
+// Aggiunta di un prodotto al catalogo
+router.post('', async (req, res) => {
+
+	let product = new Product({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        category: req.body.category,
+        tag: req.body.tag,
+        images: req.body.images
+    });
+    
+	product = await product.save();
+    
+    let productId = product.id;
+
+    console.log('Prodotto aggiunto correttamente al catalogo');
+    res.location("/api/v1/products/" + productId).status(201).send();
+});
 
 module.exports = router;
