914e4dfd42898d896303d7767871443105535f13
davidetrevisi <davide.trevisi19@gmail.com>
2022-05-13 18:28:40 +0200

    Documentazione aggiornata e fix

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const app = require('./app/app.js');
-const mongoose = require('mongoose');
-app.locals.db = mongoose.connect(process.env.DB_URL,
-{useNewUrlParser: true, useUnifiedTopology: true})
-.then ( () => {
-console.log("Connected to Database");
-app.listen(8080, () => { console.log(`Server listening`) });
-});
+const app = require("./app/app.js");
+const mongoose = require("mongoose");
+
+app.locals.db = mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to Database");
+    app.listen(8080, () => {
+      console.log(`Server listening`);
+    });
+  });
