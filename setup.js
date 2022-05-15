946d12c61aed78094f119eeab22db056d2779858
davidetrevisi <davide.trevisi19@gmail.com>
2022-05-15 10:12:45 +0200

    Rimozione parti ridondanti

--- setup.js
+++ setup.js
@@ -1,14 +1,1 @@
 const mongoose = require("mongoose");
-
-// Per usare il database in locale cambiare la stringa in .connect() con 'mongodb://localhost:27017/Test'
-// Il database deve essere stato prima avviato lanciando il programma "mongod.exe"
-
-// Test di connessione al database in remoto.
-
-mongoose
-  .connect(
-    "mongodb+srv://Admin:IngegneriaDelSoftware2022@database.0gkvd.mongodb.net/Test?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log("Connesso al Database"); // Stampa di cortesia a connessione avvenuta
-  });
